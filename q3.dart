96d02c659d21089b5813424f8d89812cfaa0ec02
riffana-lang <rifanasherin80@gmail.com>
2025-11-03 21:08:13 -0800

    Add files via upload

---
+++ q3.dart
@@ -0,0 +1,27 @@
+void main(){
+  
+  var cityTemps = {'Delhi': 39, 'Kochi': 31, 'Shimla': 15, 'Chennai': 36, 'Bangalore': 27};
+  int hot=0, moderate=0, cold=0;
+  
+  cityTemps.forEach((city,temps){
+    if(temps >=35){
+      print("$city:hot");hot++;
+      
+      
+    }else if(temps >20){
+      print("$city:moderate");moderate++;
+      
+    }else{
+      print("$city:cold");cold++;
+    }
+  });
+  
+  print("hot:$hot ,moderate:$moderate , cold :$cold"); 
+  
+  
+  
+  
+  
+  
+  
+}
